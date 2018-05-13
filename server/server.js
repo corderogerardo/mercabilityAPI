0f2c5caf0c7765de448098208b635dfcb803bfb2
Gerardo Cordero <cordero.gerard@gmail.com>
2018-05-13 12:05:48 -0400

    Development and Test Config with server.js

---
+++ server/server.js
@@ -0,0 +1,27 @@
+require('./config/config');
+// TODO-do: MongoDB Start Windows "C:\Program Files\MongoDB\Server\3.6\bin\mongod.exe"
+// Third party modules
+const express = require('express');
+const bodyParser = require('body-parser');
+
+// Client ID Google+ Oauth nof-test :
+// Client secret :
+
+// My modules exported
+let { todos } = require('./router/todos');
+let { users } = require('./router/users');
+// Express REST API Server Instance
+let app = express();
+const port = process.env.PORT;
+
+// Use Middleware to allow express read and send json data, body parser wraps express.
+app.use(bodyParser.json());
+
+app.use(users);
+app.use(todos);
+
+app.listen(port, () => {
+    console.log(`Started on port ${port}`);
+});
+
+module.exports = { app };
