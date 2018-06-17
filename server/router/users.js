d5ce2f6a2b7ff2ca6919ed9cd44dba10e4b31b2a
Gerardo Cordero <cordero.gerard@gmail.com>
2018-06-17 12:18:51 -0400

    Login user error

--- server/router/users.js
+++ server/router/users.js
@@ -168,7 +168,7 @@ users.post('/users/login', (req, res) => {
             res.header('x-auth', token).send(user);
         });
     }).catch((e) => {
-        res.status(400).send();
+        res.status(400).send(e);
     });
 });
 
